1396998795fd8aa4122b086cf02c4a4ff4f64854
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-01-11 13:02:43 +0100

    feat: add event listener to toggle overlay

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // DOM ELEMENTS
 const cardsElm = document.querySelector(".row")
 const imgElm = document.querySelector(".img-fluid")
+const overlayElm = document.querySelector(".overlay")
+const overlayImgElm = document.querySelector(".overlay-img")
 
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
     .then(function (res) {
@@ -13,12 +15,27 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
                         <img class="pin-card" src="img/pushpin.svg" alt="">
                     </div>
                     <div>
-                        <img class="img-fluid" src="${card.url}" alt="prodotto ${i + 1}">
+                        <img class="img-fluid" src="${card.url}" alt="foto ${i + 1}">
                     </div>
                     <div class="card-desc">
                         <span class="description">${card.title}</span><br>
                     </div>
                 </div>`;
-    }
-})
+        }
     
+    const postCards = document.querySelectorAll(".col")
+
+    postCards.forEach((postCard) => {
+        postCard.addEventListener("click", () => {
+            const urlImg = postCard.querySelector(".img-fluid").src;
+            overlayElm.classList.remove("hidden");
+            overlayElm.innerHTML = `<button id="overlay-img-btn">Chiudi</button>
+                                    <img class="overlay-img" src="${urlImg}" alt="">`;
+
+            const overlayBtnElm = document.getElementById("overlay-img-btn");
+            overlayBtnElm.addEventListener("click", function() {
+                overlayElm.classList.add("hidden");
+            });
+        });
+    });
+})

c36aed519c462da4ee7d40bd38bd9a6250bbd9fb
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-01-12 10:53:12 +0100

    fix: change for -> forEach

--- js/script.js
+++ js/script.js
@@ -4,27 +4,24 @@ const imgElm = document.querySelector(".img-fluid")
 const overlayElm = document.querySelector(".overlay")
 const overlayImgElm = document.querySelector(".overlay-img")
 
-axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
-    .then(function (res) {
-        const newCard = res.data; 
-        for (let i = 0; i < newCard.length; i++) {
-            const card = newCard[i];
-            cardsElm.innerHTML += 
-                `<div class="col card">
-                    <div>
-                        <img class="pin-card" src="img/pushpin.svg" alt="">
-                    </div>
-                    <div>
-                        <img class="img-fluid" src="${card.url}" alt="foto ${i + 1}">
-                    </div>
-                    <div class="card-desc">
-                        <span class="description">${card.title}</span><br>
-                    </div>
-                </div>`;
-        }
-    
+axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6').then(function (res) {
+    const newCard = res.data;
+    newCard.forEach((card, i) => {
+        cardsElm.innerHTML += 
+            `<div class="col card">
+                <div>
+                    <img class="pin-card" src="img/pushpin.svg" alt="">
+                </div>
+                <div>
+                    <img class="img-fluid" src="${card.url}" alt="foto ${i + 1}">
+                </div>
+                <div class="card-desc">
+                    <span class="description">${card.title}</span><br>
+                </div>
+            </div>`;
+    });
+       
     const postCards = document.querySelectorAll(".col")
-
     postCards.forEach((postCard) => {
         postCard.addEventListener("click", () => {
             const urlImg = postCard.querySelector(".img-fluid").src;
